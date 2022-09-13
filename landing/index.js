04e0519b5c9c1d3faf8aee82503c8e5680c41a0e
egorfaresto <egorfaresto@gmail.com>
2022-09-13 12:46:42 +0600

    fix header complete contact section

--- landing/index.js
+++ landing/index.js
@@ -2,12 +2,18 @@ const cookie = document.querySelector('.cookie'),
     cookieBtn = document.querySelector('.cookie-btn')
 
 function openCookie() {
+    setTimeout(() =>{
+        cookie.style.display = 'flex'
+    }, 1999)
     setTimeout(() => {
         cookie.style.transform = 'translateY(0%)'
         cookie.style.opacity = '1'
         cookieBtn.addEventListener('click', () => {
             cookie.style.transform = 'translateY(50%)'
             cookie.style.opacity = '0'
+            setTimeout(() => {
+                cookie.style.display = 'none'
+            }, 501)
         })
     }, 2000)
 }
